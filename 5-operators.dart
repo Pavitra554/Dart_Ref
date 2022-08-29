193f77cd16501bf99770a72fb979a4d646497af7
PAVITRA BEHARA <pavitraredmi@gmail.com>
2022-08-30 01:17:05 +0530

    ✅ Operators

---
+++ 5-operators.dart
@@ -0,0 +1,20 @@
+main() {
+  //Arithematic operators are same like other languages
+  //Logical operators are also same like other languages
+  //Ternary operators are also same other languages
+  //If-Else and Switch statements are same like other languages
+
+  //Null Aware Operator
+  //(?.)
+  //(??)(New!)
+  //(??=)(New!)
+  // var n;
+  // print(n?.num ?? 0); //if n then print n otherwise 0
+
+  // var n;
+  // print(n ??= 520); //Default value 520
+}
+
+class A {
+  int num = 10;
+}
